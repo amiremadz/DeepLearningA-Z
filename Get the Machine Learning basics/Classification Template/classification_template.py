42add5e1829b01f7d11bc07fb77515ea2cd9c29b
amiremadz <amire@ucla.edu>
2017-07-23 11:55:49 -0700

    Add Logistic Regression classifier, as an example.

--- Get the Machine Learning basics/Classification Template/classification_template.py
+++ Get the Machine Learning basics/Classification Template/classification_template.py
@@ -23,6 +23,11 @@ X_test = sc.transform(X_test)
 # Fitting classifier to the Training set
 # Create your classifier here
 
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
