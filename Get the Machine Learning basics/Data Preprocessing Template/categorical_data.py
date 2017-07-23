6b461f1ca54112c1ccebfbf927a56bf7f8d9ad61
amiremadz <amire@ucla.edu>
2017-07-22 19:05:12 -0700

    Splitting input data into training and testing sets.

--- Get the Machine Learning basics/Data Preprocessing Template/categorical_data.py
+++ Get the Machine Learning basics/Data Preprocessing Template/categorical_data.py
@@ -26,3 +26,18 @@ X = onehotencoder.fit_transform(X).toarray()
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+
+
