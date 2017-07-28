0b791c77dab8df8cc910cb8ebc6966af34e3babd
Amir <aemadzadeh@nvidia.com>
2017-07-28 14:18:02 -0700

    Some reshuffling, and add improve file.

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
@@ -52,6 +52,8 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
+
+# Image size can be increased from 64
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
@@ -67,3 +69,22 @@ classifier.fit_generator(training_set,
                          epochs = 25,
                          validation_data = test_set,
                          validation_steps = 2000)
+
+# Predict
+
+from keras.preprocessing import image 
+import numpy as np
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_2.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image) # (64, 64, 3)
+test_image = np.expand_dims(test_image, axis = 0) # input must be in batch: (1, 64, 64 , 3)
+
+print training_set.class_indices
+
+prediction = classifier.predict(test_image)
+print prediction
+
+if(prediction[0][0]==1):
+  print 'dog'
+else:  
+  print 'cat'
--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 9 - Improving the CNN/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 9 - Improving the CNN/cnn.py
@@ -17,6 +17,7 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import Dropout
 
 # Initialising the CNN
 classifier = Sequential()
@@ -32,22 +33,22 @@ classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Adding a third convolutional layer
-# classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
-# classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
+classifier.add(Conv2D(64, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 64, activation = 'relu'))
+classifier.add(Dropout(0.4))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Part 2 - Fitting the CNN to the images
-
+batch_size = 32
 from keras.preprocessing.image import ImageDataGenerator
 
 train_datagen = ImageDataGenerator(rescale = 1./255,
@@ -57,18 +58,41 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
+
+# Image size can be increased from 64
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
-                                                 batch_size = 32,
+                                                 batch_size = batch_size,
                                                  class_mode = 'binary')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
+                                            batch_size = batch_size,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
+                         steps_per_epoch = 8000/batch_size,
+                         epochs = 50,
                          validation_data = test_set,
-                         validation_steps = 2000)
+                         validation_steps = 2000/batch_size,
+                         workers = 12,
+                         max_queue_size = 100)
+
+# Predict
+
+from keras.preprocessing import image 
+import numpy as np
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_2.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image) # (64, 64, 3)
+test_image = np.expand_dims(test_image, axis = 0) # input must be in batch: (1, 64, 64 , 3)
+
+print training_set.class_indices
+
+prediction = classifier.predict(test_image)
+print prediction
+
+if(prediction[0][0]==1):
+  print 'dog'
+else:  
+  print 'cat'
